b037ac2e5cd95c0ff23495373b78478f3706abb5
surdarkweb <ben639405@gmail.com>
2025-01-03 21:44:32 +0100

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,4 +1,4 @@
-const GITHUB_AUTH_TOKEN = "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"
+const GITHUB_AUTH_TOKEN = "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"
 const BOT_NUMBER = "2348101699340"
 const SESSION_ID = "PRABATH-MD~eKokmJxI#rfAFjrwaIW4tRTkDTpjJbJ46wbkoQwN7AETyvDxOkCc"
 
